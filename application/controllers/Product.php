ed11ab22252a3c9614e5a3a62a990d92614f2a25
Haocheng Liu <liuhc@staff.zjutv.com>
2015-12-16 15:22:50 +0800

    update 12.16 15:22

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -7,7 +7,8 @@ class Product extends CI_Controller {
 	{
 		$data['mytitle'] = '产品介绍 | 杭州冠啸电子科技有限公司';
 		$data['href'] = 'product';
-		
+		$data['location'] = '产品介绍';
+		$data['banner_intro'] = "你好呀";
 		$this->load->helper('url');
 		$this->load->view('product.html',$data);
 	}
