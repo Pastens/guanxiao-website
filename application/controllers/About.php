a9094892e0f19d870eca35baea0063af7f9cd9dd
Haocheng Liu <liuhc@staff.zjutv.com>
2015-12-21 22:01:54 +0800

    update 12.21 22:01

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -6,6 +6,8 @@ class About extends CI_Controller {
 	public function index()
 	{
 		$data['mytitle'] = '关于我们 | 杭州冠啸电子科技有限公司';
+		$data['href'] = 'about';
+
 		$this->load->helper('url');
 		$this->load->view('home.html',$data);
 	}
--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -6,6 +6,8 @@ class Contact extends CI_Controller {
 	public function index()
 	{
 		$data['mytitle'] = '联系我们 | 杭州冠啸电子科技有限公司';
+		$data['href'] = 'contact';
+
 		$this->load->helper('url');
 		$this->load->view('home.html',$data);
 	}
