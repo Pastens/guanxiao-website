aa7d024178c82775347666d7c8e95495233277a0
Haocheng Liu <liuhc@staff.zjutv.com>
2015-12-21 13:19:48 +0800

    update 12.21 13:19

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -19,6 +19,6 @@ class Product extends CI_Controller {
 		$data['location'] = '产品目录';
 		$data['banner_intro'] = "创新需要载体，创新平台可以让中学生与当前高新技术进行亲密接触，更可以让他们利用创新平台进行自主的创新性设计与拓展。学校可以进一步激发中学生的学习兴趣和创新激情，凝聚出更多创新科研成果。";
 		$this->load->helper('url');
-		$this->load->view('product.html',$data);
+		$this->load->view('product-catalog.html',$data);
 	}
 }
