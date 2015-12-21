27f130af6df2a8a3b75d6dbf231959d00b41c2c4
Haocheng Liu <liuhc@staff.zjutv.com>
2015-12-22 00:14:35 +0800

    update 12.22 00:14

--- application/views/product/catalog.php
+++ application/views/product/catalog.php
@@ -1,6 +1,109 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
     <section class="catalog container-fluid">
     	<div class="container">
-    		
+    		<div class="row">
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>GXCOA平台</h3>
+	    					<p>完成基本实验使用</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>GXCOB平台</h3>
+	    					<p>完成扩展实验使用</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>GXSSA平台</h3>
+	    					<p>可实现检测各种环境参数</p>
+	    				</div>
+	    			</a>
+	    		</div>
+    		</div>
+    		<div class="row">
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>GXUDL平台</h3>
+	    					<p>实现程序更新与下载</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>六自由度机械臂</h3>
+	    					<p>可编程控制的机器臂</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>智能手套</h3>
+	    					<p>可感知力的转动、手指压力</p>
+	    				</div>
+	    			</a>
+	    		</div>
+    		</div>
+    		<div class="row">
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>智能小车</h3>
+	    					<p>可编程控制的循迹避障智能小车</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>TFT真彩触摸屏</h3>
+	    					<p>可编程电阻触摸屏</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>LED闪字风扇灯</h3>
+	    					<p>可编程变速变字风扇灯</p>
+	    				</div>
+	    			</a>
+	    		</div>
+    		</div>
+    		<div class="row">
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>脑电信号采集仪</h3>
+	    					<p>可检测人脑信号的专业设备</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>夜空表演服</h3>
+	    					<p>程控图案的舞台表演服</p>
+	    				</div>
+	    			</a>
+	    		</div>
+	    		<div class="col-xs-4">
+	    			<a href="#" class="thumbnail">
+	    				<div class="caption">
+	    					<h3>F-LED灯</h3>
+	    					<p>可编程可调色防水LED灯</p>
+	    				</div>
+	    			</a>
+	    		</div>
+    		</div>
     	</div>
     </section>
